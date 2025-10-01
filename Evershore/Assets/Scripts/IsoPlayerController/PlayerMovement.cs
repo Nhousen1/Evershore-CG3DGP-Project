9ba7e3a7866bea09f2a48a40bdcec5551bd105f0
Mking655 <53408059+Mking655@users.noreply.github.com>
2025-10-01 18:27:49 -0400

    Now using character controller system
    added jumping

--- Evershore/Assets/Scripts/IsoPlayerController/PlayerMovement.cs
+++ Evershore/Assets/Scripts/IsoPlayerController/PlayerMovement.cs
@@ -3,12 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float walkSpeed;
     public float runSpeedMultiplier;
+    public float gravity = -9.8f;
+    public float jumpHeight;
 
     private Vector2 moveInput;
     private bool isRunning;
@@ -18,17 +21,34 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 isoForward;
     private Vector3 isoRight;
 
+    private CharacterController cc;
+
+    [SerializeField]
+    private bool canMove;
+
+    private Vector3 velocity;
     public void OnMove(InputValue value)
     {
-        moveInput = value.Get<Vector2>();
-        moveInput.Normalize();
+        if (canMove)
+        {
+            moveInput = value.Get<Vector2>();
+            moveInput.Normalize();
+        }
     }
     public void OnSprint(InputValue value)
     {
         isRunning = value.isPressed;
     }
+    public void OnJump(InputValue value)
+    {
+        //jump conditions
+        if (!cc.isGrounded || !canMove) { return; }
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
     public void Start()
     {
+        canMove = true;
+        cc = GetComponent<CharacterController>();
         //UNDER THIS SETUP, THE CAMERA SHOULD NEVER ROTATE
         if (isoCam != null) 
         {
@@ -45,8 +65,30 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        Vector3 move = isoForward * moveInput.x + isoRight * moveInput.y;
         float speed = walkSpeed * (isRunning ? runSpeedMultiplier : 1f);
-        transform.Translate(move * speed * Time.deltaTime, Space.World);
+
+        Vector3 move = isoForward * moveInput.x + isoRight * moveInput.y;
+        move = new Vector3(move.x * speed * Time.deltaTime, 0, move.z * speed * Time.deltaTime);
+        cc.Move(move);
+
+        // gravity
+        if (cc.isGrounded && velocity.y < 0)
+        {
+            // Reset downward velocity when grounded
+            velocity.y = -2f;
+        }
+
+        // Apply gravity every frame
+        velocity.y += gravity * Time.deltaTime;
+
+        cc.Move(velocity * Time.deltaTime);
+    }
+    public void stopInputMovement()
+    {
+        canMove = false;
+    }
+    public void unstopInputMovement()
+    {
+        canMove = true;
     }
 }
