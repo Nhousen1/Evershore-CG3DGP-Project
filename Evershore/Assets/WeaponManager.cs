61a4f7fc37fb3f2e9e724da0bfdf0115adf44470
Mking655 <53408059+Mking655@users.noreply.github.com>
2025-10-01 23:14:14 -0400

    Aiming

---
+++ Evershore/Assets/WeaponManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    //list weapons
+    //indexing based on scroll wheel
+    //call use events
+}
+
